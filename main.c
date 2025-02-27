35a9e09577b665aaccb5ed35e6b368b379575eeb
AbdelrahmanAmgad201 <abdelrahmanamgad04@gmail.com>
2025-02-27 09:56:51 +0200

    added external commands and CD command

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "string_parsing.h"
 
 #define MAXSIZE 1024
-#define MAXARGS 50 
-
-
 
 int check_instruction(char **args) {
     if (strcmp(args[0], "exit") == 0) {
@@ -16,15 +16,52 @@ int check_instruction(char **args) {
         return 1;  // Built-in command
     } 
     else {
-        return 2;  //use execvp
+        return 2;  // Use execvp
+    }
+    return 3;
+}
 
+void execute_external(char **args) {
+    int pid = fork();
+    if (pid < 0) {
+        printf("Error executing command\n");
+    } else if (pid == 0) {
+        printf("Hello from child process (before execvp)\n");
+        execvp(args[0], args);
+        perror("execvp failed");  // Print error if execvp fails
+        exit(1);
+    } else {
+        printf("Hello from parent\n");
+        waitpid(pid, NULL, 0);  // Correct waitpid usage
+    }
+}
+
+void execute_built_in(char **args) {
+    if (strcmp(args[0], "cd") == 0) {
+        if (args[1] == NULL) {
+            printf("cd: missing argument\n");
+        } else {
+            if (chdir(args[1]) != 0) {
+                perror("cd failed");
+            }
+        }
+    } else if (strcmp(args[0], "echo") == 0) {
+        for (int i = 1; args[i] != NULL; i++) {
+            printf("%s ", args[i]);
+        }
+        printf("\n");
+    } else if (strcmp(args[0], "export") == 0) {
+        // Placeholder for future export functionality
+        printf("export command not implemented yet\n");
+    } else {
+        printf("Unknown built-in command\n");
     }
 }
 
 void shell() {
     char input[MAXSIZE];
     char **args;
-
+    chdir(getenv("HOME"));
     printf("WELCOME TO MY SHELL\n");
 
     while (1) {
@@ -39,23 +76,19 @@ void shell() {
         print_string_array(args);
         int instruction_type = check_instruction(args);
 
-        switch (instruction_type)
-        {
-        case 0:
-            exit(0);
-            break;
-        
-        case 1:
-            // built in commands;
-            break;
-        case 2:
-            // execute
-            break;
-        default:
-            printf("unknown command, try again");
-        
-        execvp(args[0], args);
-        // Free allocated memory
+        switch (instruction_type) {
+            case 0:
+                exit(0);
+                break;
+            case 1:
+                execute_built_in(args);
+                break;
+            case 2:
+                execute_external(args);
+                break;
+            default:
+                printf("Unknown command, try again\n");
+        }
         
         for (int i = 0; args[i] != NULL; i++) {
             free(args[i]);
